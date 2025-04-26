5b227d596ba1a9bd91f30a7eff47e3a5f9f8ded0
Luo1imasi <2488288779@qq.com>
2025-04-26 16:25:41 +0800

    nice

--- src/motors/src/motors_node.cpp
+++ src/motors/src/motors_node.cpp
@@ -134,11 +134,11 @@ class MotorsNode : public rclcpp::Node {
             {
                 std::scoped_lock lock(left_mutex_, right_mutex_);
                 for (int i = 0; i < 6; i++) {
-                    left_motors[i]->MotorMitModeCmd(0, 0, 50, 1, 0);
-                    right_motors[i]->MotorMitModeCmd(0, 0, 50, 1, 0);
+                    left_motors[i]->MotorMitModeCmd(0, 0, 50, 2, 0);
+                    right_motors[i]->MotorMitModeCmd(0, 0, 50, 2, 0);
                     Timer::ThreadSleepFor(1);
                 }
-                Timer::ThreadSleepFor(500);
+                Timer::ThreadSleepFor(2000);
                 for (int i = 0; i < 6; i++) {
                     left_motors[i]->MotorMitModeCmd(0, 0, kp_[i], kd_[i], 0);
                     right_motors[i]->MotorMitModeCmd(0, 0, kp_[i], kd_[i], 0);
